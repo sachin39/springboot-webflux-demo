71b9ab25ec38c930c37b734d775df80340a15354
sachin39 <sachin.kr.39@gmail.com>
2022-02-18 18:32:48 +0530

    updated test for Flux example

--- src/test/java/com/reactiveprogramming/webflux/MonoFluxTest.java
+++ src/test/java/com/reactiveprogramming/webflux/MonoFluxTest.java
@@ -23,14 +23,36 @@ public class MonoFluxTest {
 
         //Subscriber subscribe to publisher
         //subscribe is an overloaded method so we can also print exception
-        stream1.subscribe(System.out::println, e-> System.out.println(e.getMessage()));
+        stream1.subscribe(System.out::println, e -> System.out.println(e.getMessage()));
     }
 
     @Test
-    public void testFlux(){
-        //Create a publisher with number of data (stream)
-        Flux<String> just = Flux.just("Spring", "Spring boot", "Hibernate", "JPA");
+    public void testFlux() {
+        //Create a publisher with number of data (stream) and log the events
+        //there will be 4 onNext() calls because we have stream of 4 objects
+        Flux<String> fluxString = Flux.just("Spring", "Spring boot", "Hibernate", "JPA")
+                //concat one more string onNext calls will increase by 1
+                .concatWithValues("AWS")
+                .log();
+        //after 5 onNext() calls we onComplete() method would be called upon successful completion
+        //Subscriber subscribe to publisher
+        fluxString.subscribe(System.out::println);
+    }
 
-        //subscribe to publisher
+    @Test
+    public void testFluxWithException() {
+        //Create a publisher with number of data (stream) and log the events
+        //there will be 4 onNext() calls because we have stream of 4 objects
+        Flux<String> fluxString = Flux.just("Spring", "Spring boot", "Hibernate", "JPA")
+                //concat one more string onNext calls will increase by 1
+                .concatWithValues("AWS")
+                .concatWith(Flux.error(new RuntimeException("exception occurred in Flux.")))
+                .concatWithValues("Cloud")
+                .log();
+        //after 5 onNext() calls we onError() method would be called upon Exception case.
+        //Flux throws error so "Cloud" will not be concatenated to fluxString stream because an exception has been occurred.
+
+        //Subscriber subscribe to publisher
+        fluxString.subscribe(System.out::println, e -> System.out.print(e.getMessage()));
     }
 }
